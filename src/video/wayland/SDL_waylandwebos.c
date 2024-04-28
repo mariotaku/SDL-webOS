a08e76252ce809bece3047c09e75f8342914575f
Ningyuan Li <mariotaku.lee@gmail.com>
2024-04-29 00:56:13 +0900

    added _WEBOS_ACCESS_POLICY_KEYS_GUIDE

--- src/video/wayland/SDL_waylandwebos.c
+++ src/video/wayland/SDL_waylandwebos.c
@@ -37,6 +37,7 @@ static const char* webos_window_hints[] = {
     SDL_HINT_WEBOS_ACCESS_POLICY_KEYS_BACK,
     SDL_HINT_WEBOS_ACCESS_POLICY_KEYS_EXIT,
     SDL_HINT_WEBOS_ACCESS_POLICY_KEYS_HOME,
+    SDL_HINT_WEBOS_ACCESS_POLICY_KEYS_GUIDE,
     SDL_HINT_WEBOS_CURSOR_CALIBRATION_DISABLE,
     SDL_HINT_WEBOS_CURSOR_FREQUENCY,
     SDL_HINT_WEBOS_CURSOR_SLEEP_TIME,
@@ -124,6 +125,9 @@ int WaylandWebOS_SetupSurface(_THIS, SDL_WindowData *data)
     if (SDL_GetHintBoolean(SDL_HINT_WEBOS_ACCESS_POLICY_KEYS_HOME, SDL_FALSE)) {
         wl_webos_shell_surface_set_property(data->shell_surface.webos.webos, "_WEBOS_ACCESS_POLICY_KEYS_HOME", "true");
     }
+    if (SDL_GetHintBoolean(SDL_HINT_WEBOS_ACCESS_POLICY_KEYS_GUIDE, SDL_FALSE)) {
+        wl_webos_shell_surface_set_property(data->shell_surface.webos.webos, "_WEBOS_ACCESS_POLICY_KEYS_GUIDE", "true");
+    }
     if(SDL_GetHintBoolean(SDL_HINT_WEBOS_CURSOR_CALIBRATION_DISABLE, SDL_FALSE)) {
         wl_webos_shell_surface_set_property(data->shell_surface.webos.webos, "restore_cursor_position", "true");
     }
@@ -209,6 +213,9 @@ static void WindowHintsCallback(void *userdata, const char *name, const char *ol
                                             SDL_GetStringBoolean(newValue, SDL_FALSE) ? "true" : "false");
     } else if (SDL_strcmp(name, SDL_HINT_WEBOS_ACCESS_POLICY_KEYS_HOME) == 0) {
         wl_webos_shell_surface_set_property(win_data->shell_surface.webos.webos, "_WEBOS_ACCESS_POLICY_KEYS_HOME",
+                                            SDL_GetStringBoolean(newValue, SDL_FALSE) ? "true" : "false");
+    } else if (SDL_strcmp(name, SDL_HINT_WEBOS_ACCESS_POLICY_KEYS_GUIDE) == 0) {
+        wl_webos_shell_surface_set_property(win_data->shell_surface.webos.webos, "_WEBOS_ACCESS_POLICY_KEYS_GUIDE",
                                             SDL_GetStringBoolean(newValue, SDL_FALSE) ? "true" : "false");
     } else if (SDL_strcmp(name, SDL_HINT_WEBOS_CURSOR_CALIBRATION_DISABLE) == 0) {
         wl_webos_shell_surface_set_property(win_data->shell_surface.webos.webos, "restore_cursor_position",
