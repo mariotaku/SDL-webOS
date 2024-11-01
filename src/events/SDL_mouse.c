6c0e87f6ede8f935244b63209da63981db153f3f
Ningyuan Li <mariotaku.lee@gmail.com>
2024-11-01 23:49:01 +0900

    improved hidden cursor creation

--- src/events/SDL_mouse.c
+++ src/events/SDL_mouse.c
@@ -243,7 +243,7 @@ void SDL_MousePostInit(void)
     }
 #ifdef __WEBOS__
     {
-        SDL_Surface *surface = SDL_CreateRGBSurfaceWithFormat(0, 1, 1, 32, SDL_PIXELFORMAT_RGBA32);
+        SDL_Surface *surface = SDL_CreateRGBSurfaceWithFormat(0, 1, 1, 32, SDL_PIXELFORMAT_ARGB8888);
         SDL_memset(surface->pixels, 0, (size_t)surface->h * surface->pitch);
         mouse->hidden_cursor = SDL_CreateColorCursor(surface, 0, 0);
         SDL_FreeSurface(surface);
